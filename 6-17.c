8d08d267d82133493494cdb0ced881c8999948f6
euna <127270717+chna8137@users.noreply.github.com>
2023-06-09 17:13:10 +0900

    23-06-09

---
+++ 6-17.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+	int i, k;
+
+	for(k = 2; k <= 9; k++){
+		printf(" #Á¦%d´Ü#    ",k);
+	}printf("\n\n");
+
+	for(i = 1; i <= 9; i++){
+		for(k = 2; k <= 9; k++){
+			printf("%2d x %2d = %2d", k , i, k*i);
+		}
+		printf("\n");
+	}
+}
+	
